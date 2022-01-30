c4a26caba9f2319ba2a46ec7f7361607ecbe12bf
Arcaz22 <chandraarcychan@gmail.com>
2022-01-31 01:22:11 +0700

    tugas akhir Dasar Pemrograman Web Dicoding

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+function lengkap(obj) {
+  obj.innerHTML = "Chandra Arcychan Azfar"
+}
+
+function nama(obj) {
+  obj.innerHTML = "Me"
+}
+
+function italic(){
+  document.getElementById("content").style.fontStyle = "italic";
+}
